27f0a21545653fa32a32ffd20010a040bf6bc620
pabitra <89703515+appu900@users.noreply.github.com>
2024-12-25 17:19:56 +0530

    added express config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
+import app from "./app";
+import { Config } from "./config";
 
+const startServer = async () => {
+  const PORT = Config.PORT || 5000;
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-
-function welcome(name:string){
-    console.log(`Welcome ${name}`);
-}
-
-welcome('Sandeep');
+startServer();
