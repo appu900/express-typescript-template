f2301bcbcc469816726448fad8975ac6aa0472c9
pabitra <89703515+appu900@users.noreply.github.com>
2024-12-25 18:09:50 +0530

    add errorhandler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import logger from "./config/logger";
+import createHttpError, { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Authservice pinged");
+app.get("/", async (req, res, next) => {
+  const error = createHttpError(404, "Route not found");
+  next(error);
+  // res.send("Authservice pinged");
+});
+
+// global error handler
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
